7a32e0f514f6d1eb12d4eb531d6892dfaab71112
dank <dank@danks.local>
2025-10-14 17:46:58 -0400

    day1

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # flatGrad
 Another neural network library in Python. I wouldnt use this. Looking to learn something here.
+
+
+# Install
+If you still are interested you can probably install by running the following commands.
+
+    - python3 -m venv env && source env/bin/activate
+    - git clone https://github.com/newpolygons/flatgrad.git
+    - cd flatgrad
+    - python3 -m pip install -e  .
+
+# Credits 
+    - Micrograd (Repo is currently basically a clone until I learn to implement things)
---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open('README.md', 'r') as f:
+    longdesc = f.read()
+
+
+setuptools.setup(
+    name='flatgrad',
+    version='0.0.1',
+    author='newpolygons',
+    description='Another neural network library in Python. I wouldnt use this. Looking to learn something here.',
+    long_description=longdesc, 
+    long_description_content_type='text/markdown', 
+    url='https://github.com/newpolygons/flatgrad',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "graphviz",
+
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Lisence :: GPLv3',
+        'Operating System :: No Bill Gates',
+    ],
+    python_requires='>=3.10',
+
+)
